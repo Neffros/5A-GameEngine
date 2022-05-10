b21dda7ab790a137535a109e1e559d7a62040153
sikny <pamartqu@gmail.com>
2022-05-10 11:15:14 +0200

    systemmanager

--- engine/headers/SystemManager.h
+++ engine/headers/SystemManager.h
@@ -1,9 +1,18 @@
 #pragma once
 
-namespace GameEngine
-{
-	class SystemManager
-	{
+#include <map>
+#include "ComponentManager.h"
+#include "System.h"
+#include "RTTI.h"
 
-	};
+namespace GameEngine {
+    template<class T>
+    class SystemManager {
+    private:
+        std::map<ComponentSignature, std::unique_ptr<System<T>>> _signatureToSystems;
+        std::vector<std::unique_ptr<System<T>>> _systems;
+    public:
+        void registerSystem(ComponentSignature signature);
+        void registerSystem(RTTI<T> rtti, ComponentSignature signature);
+    };
 }
--- engine/src/SystemManager.cpp
+++ engine/src/SystemManager.cpp
@@ -1,3 +1,15 @@
 #include "../headers/SystemManager.h"
 
 using namespace GameEngine;
+
+template<class T>
+void SystemManager<T>::registerSystem(ComponentSignature signature) {
+    auto system = std::make_unique<System<T>>();
+    _systems.push_back(system);
+    _signatureToSystems.emplace({signature, system});
+}
+
+template<class T>
+void SystemManager<T>::registerSystem(RTTI<T> rtti, ComponentSignature signature) {
+
+}
