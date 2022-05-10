c9f819ffbaf83e24da376aec01ed21d26128a60d
Quentin <pamartqu@gmail.com>
2022-05-11 01:17:59 +0200

    jobs id -> wait for job to finish method

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -2,35 +2,19 @@
 
 using namespace GameEngine;
 
+int ThreadPool::QueueJob(const std::function<void()> &job) {
+    std::unique_lock<std::mutex> lock(_queueMutex);
+    _idToStatus[_lastJobId] = false;
+    _jobs.push(Job(_lastJobId, job));
+    return _lastJobId++;
+}
+
 void ThreadPool::Start() {
     const unsigned int nThreads = std::thread::hardware_concurrency();
     _threads.resize(nThreads);
     for (int i = 0; i < nThreads; ++i) {
         _threads.at(i) = std::thread(&ThreadPool::ThreadLoop, this);
     }
-}
-
-void ThreadPool::ThreadLoop() {
-    while (true) {
-        std::function <void()> job;
-        {
-            std::unique_lock<std::mutex> lock(_queueMutex);
-            _mutexCondition.wait(lock, [this] {
-                return !_jobs.empty() || _shouldTerminate;
-            });
-            if (_shouldTerminate) {
-                return;
-            }
-            job = _jobs.front();
-            _jobs.pop();
-        }
-        job();
-    }
-}
-
-void ThreadPool::QueueJob(const std::function<void()> &job) {
-    std::unique_lock<std::mutex> lock(_queueMutex);
-    _jobs.push(job);
 }
 
 void ThreadPool::Stop() {
@@ -42,4 +26,27 @@ void ThreadPool::Stop() {
         activeThread.join();
     }
     _threads.clear();
+}
+
+void ThreadPool::ThreadLoop() {
+    while (true) {
+        Job job;
+        {
+            std::unique_lock<std::mutex> lock(_queueMutex);
+            _mutexCondition.wait(lock, [this] {
+                return !_jobs.empty() || _shouldTerminate;
+            });
+            if (_shouldTerminate) {
+                return;
+            }
+            job = _jobs.front();
+            _jobs.pop();
+        }
+        job.func();
+        _idToStatus[job.id] = true;
+    }
+}
+
+void ThreadPool::WaitForJob(int jobId) {
+    while(!_idToStatus[jobId]);
 }
